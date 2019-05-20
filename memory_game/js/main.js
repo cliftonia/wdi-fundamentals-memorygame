bd605dceef130769819130f2ffe9804fb8dfeb24
Clifton Baggerman <cliftonbaggerman@Cliftons-MacBook-Pro.local>
2019-05-20 14:16:45 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Flipped " + cardsInPlay);
+cardsInPlay.pop();
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User Flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+
+} else {
+	
+}
