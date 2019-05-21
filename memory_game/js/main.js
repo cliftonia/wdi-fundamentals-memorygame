1dbdb9f9bbee29ad803a672f6f4ef16c6a42631f
Clifton Baggerman <cliftonbaggerman@Cliftons-MacBook-Pro.local>
2019-05-21 14:56:41 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped " + cardsInPlay);
-cardsInPlay.pop();
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardsInPlay);
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
+	} else {
+	  console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId) {
+	cardsInPlay.pop();
+	console.log("User flipped" + cards[cardId]);
+}
+
+
 
 if (cardsInPlay.length === 2) {
 
 } else {
-	
+
 }
