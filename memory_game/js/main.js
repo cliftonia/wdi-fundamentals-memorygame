197d1177741675b7551888151cc32a5c1e1cd92b
Clifton Baggerman <cliftonbaggerman@Cliftons-MacBook-Pro.local>
2019-05-29 14:05:29 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,7 +33,7 @@ function checkForMatch() {
 
 function flipCard(cardId) {
 	cardsInPlay.pop();
-	console.log("User flipped" + cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
 }
 
 
